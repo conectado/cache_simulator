f4df5b8efdd2e3d7ecc870b842104f33a571857d
conectado <gabrielalejandro7@gmail.com>
2019-04-30 22:31:02 -0300

    Update tests into a different file

--- src/memory/mod.rs
+++ src/memory/mod.rs
@@ -77,21 +77,21 @@ where
     }
 }
 
-pub struct CacheDirect<T>
+pub struct CacheDirect<T, U = u32>
 where
     T: std::clone::Clone,
 {
     internal_memory: Vec<CacheMemory<T, u32>>,
-    cache_size: u32,
-    line_size: u32,
-    address_size: u32,
+    cache_size: U,
+    line_size: U,
+    address_size: U,
 }
 
-impl<T> Accessable for CacheDirect<T>
+impl<T, U> Accessable for CacheDirect<T, U>
 where
     T: std::clone::Clone,
 {
-    type Address = u32;
+    type Address = U;
     type Data = T;
 
     fn get(&mut self, pos: Self::Address) -> Option<Self::Data> {
@@ -121,7 +121,7 @@ where
     }
 }
 
-impl<T> CacheDirect<T>
+impl<T, U> CacheDirect<T, U>
 where
     T: std::clone::Clone,
 {
@@ -152,107 +152,4 @@ fn num_bits(x: u32) -> u32 {
 }
 
 #[cfg(test)]
-mod tests {
-    use crate::memory::memory::*;
-    #[test]
-    fn correct_tag_mask() {
-        let mem_test: CacheDirect<u32> = CacheDirect {
-            internal_memory: vec![],
-            cache_size: 524288,
-            line_size: 4,
-            address_size: 24,
-        };
-
-        assert!(mem_test.calc_tag_mask() == 0xFF0000u32);
-    }
-
-    #[test]
-    fn correct_word_mask() {
-        let mem_test: CacheDirect<u32> = CacheDirect {
-            internal_memory: vec![],
-            cache_size: 524288,
-            line_size: 4,
-            address_size: 24,
-        };
-
-        assert!(mem_test.calc_word_mask() == 0x3u32);
-    }
-
-    #[test]
-    fn correct_line_mask() {
-        let mem_test: CacheDirect<u32> = CacheDirect {
-            internal_memory: vec![],
-            cache_size: 524288,
-            line_size: 4,
-            address_size: 24,
-        };
-
-        assert!(mem_test.calc_line_mask() == 0xFFFCu32);
-    }
-
-    #[test]
-    fn correct_get() {
-        let mut mem_test: CacheDirect<u32> = CacheDirect {
-            internal_memory: vec![
-                CacheMemory {
-                    data: 1,
-                    tag: 0b1u32,
-                },
-                CacheMemory {
-                    data: 2,
-                    tag: 0b0u32,
-                },
-            ],
-            cache_size: 16,
-            line_size: 1,
-            address_size: 2,
-        };
-
-        if let Some(x) = mem_test.get(0b10) {
-            assert!(x == 1);
-        } else {
-            assert!(false);
-        }
-
-        assert!(mem_test.get(0b00).is_none());
-
-        if let Some(x) = mem_test.get(0b01) {
-            assert!(x == 2);
-        } else {
-            assert!(false);
-        }
-
-        assert!(mem_test.get(0b11).is_none());
-    }
-
-    #[test]
-    fn correct_set() {
-        let mut mem_test: CacheDirect<u32> = CacheDirect {
-            internal_memory: vec![
-                CacheMemory { tag: 0, data: 0 },
-                CacheMemory { tag: 0, data: 0 },
-            ],
-            cache_size: 16,
-            line_size: 1,
-            address_size: 2,
-        };
-
-        mem_test.set(0b10, 1);
-        mem_test.set(0b01, 2);
-        if let Some(x) = mem_test.get(0b10) {
-            assert!(x == 1);
-        } else {
-            assert!(false);
-        }
-
-        assert!(mem_test.get(0b00).is_none());
-
-        if let Some(x) = mem_test.get(0b01) {
-            assert!(x == 2);
-        } else {
-            assert!(false);
-        }
-
-        assert!(mem_test.get(0b11).is_none());
-    }
-}
+pub mod tests;
---
+++ src/memory/tests.rs
@@ -0,0 +1,102 @@
+use super::*;
+#[test]
+fn correct_tag_mask() {
+    let mem_test: CacheDirect<u32> = CacheDirect {
+        internal_memory: vec![],
+        cache_size: 524288,
+        line_size: 4,
+        address_size: 24,
+    };
+
+    assert!(mem_test.calc_tag_mask() == 0xFF0000u32);
+}
+
+#[test]
+fn correct_word_mask() {
+    let mem_test: CacheDirect<u32> = CacheDirect {
+        internal_memory: vec![],
+        cache_size: 524288,
+        line_size: 4,
+        address_size: 24,
+    };
+
+    assert!(mem_test.calc_word_mask() == 0x3u32);
+}
+
+#[test]
+fn correct_line_mask() {
+    let mem_test: CacheDirect<u32> = CacheDirect {
+        internal_memory: vec![],
+        cache_size: 524288,
+        line_size: 4,
+        address_size: 24,
+    };
+
+    assert!(mem_test.calc_line_mask() == 0xFFFCu32);
+}
+
+#[test]
+fn correct_get() {
+    let mut mem_test: CacheDirect<u32> = CacheDirect {
+        internal_memory: vec![
+            CacheMemory {
+                data: 1,
+                tag: 0b1u32,
+            },
+            CacheMemory {
+                data: 2,
+                tag: 0b0u32,
+            },
+        ],
+        cache_size: 16,
+        line_size: 1,
+        address_size: 2,
+    };
+
+    if let Some(x) = mem_test.get(0b10) {
+        assert!(x == 1);
+    } else {
+        assert!(false);
+    }
+
+    assert!(mem_test.get(0b00).is_none());
+
+    if let Some(x) = mem_test.get(0b01) {
+        assert!(x == 2);
+    } else {
+        assert!(false);
+    }
+
+    assert!(mem_test.get(0b11).is_none());
+}
+
+#[test]
+fn correct_set() {
+    let mut mem_test: CacheDirect<u32> = CacheDirect {
+        internal_memory: vec![
+            CacheMemory { tag: 0, data: 0 },
+            CacheMemory { tag: 0, data: 0 },
+        ],
+        cache_size: 16,
+        line_size: 1,
+        address_size: 2,
+    };
+
+    mem_test.set(0b10, 1);
+    mem_test.set(0b01, 2);
+    if let Some(x) = mem_test.get(0b10) {
+        assert!(x == 1);
+    } else {
+        assert!(false);
+    }
+
+    assert!(mem_test.get(0b00).is_none());
+
+    if let Some(x) = mem_test.get(0b01) {
+        assert!(x == 2);
+    } else {
+        assert!(false);
+    }
+
+    assert!(mem_test.get(0b11).is_none());
+}
